b6c4ebcb83bfca474d9b6b7a890be1651ba78380
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-09-23 16:10:07 +0700

    feat(package): :sparkles: add tailwindcss

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,8 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+
+    <div class="w-16 h-16 bg-yellow-400 text-dark font-bold">yoi mamen</div>
   </q-page>
 </template>
 
